f713f4836d6eb0c286374d39d8f37d923f698fc0
Leonardo <redocs@gmail.com>
2024-05-22 23:39:05 +0200

    screen mancante

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,7 +32,7 @@ export default function Layout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="home" />
+      <Stack />
     </ThemeProvider>
   );
 }
